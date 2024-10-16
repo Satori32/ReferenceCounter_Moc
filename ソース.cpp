3bd369dcb40e7b7ceb3c8b493324826b57c75e08
Satori32 <Huga8989+githubf@gmail.com>
2024-10-16 21:45:48 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+struct ReferenceCounter {
+	intmax_t* P = NULL;
+};
+
+ReferenceCounter ConstructReferenceCounter() {
+	ReferenceCounter R;
+	R.P = (intmax_t*)calloc(1, sizeof(R.P));
+	(*R.P) = 0;
+	return R;
+}
+
+bool Inc(ReferenceCounter& In) {
+	if (In.P == NULL) { return false; }
+	(*In.P)++;
+
+	return true;
+}
+bool Dec(ReferenceCounter& In) {
+	if (In.P == NULL) { return false; }
+	(*In.P)--;
+
+	return true;
+}
+bool IsUnderZero(ReferenceCounter& In) {
+	if (In.P == NULL) { return true; }
+	if (*In.P <= 0) { return true; }
+
+	return false;
+}
+intmax_t Now(ReferenceCounter& In) {
+	if (In.P == NULL) { return 0; }
+	return *In.P;
+}
+
+bool Free(ReferenceCounter& In) {
+	if (IsUnderZero(In) == false) { return false; }
+	free(In.P);
+	In.P = NULL;
+	return true;
+}
+
+int main() {
+	ReferenceCounter R =ConstructReferenceCounter();
+	Inc(R);
+	Free(R);
+	Dec(R);
+	Free(R);
+	return 0;
+}
